8b76afd01642a58d12de43eae880b1e41bfb8902
Garett Takeyasu <takeyasu.garett@gmail.com>
2025-02-25 10:47:06 +0900

    initial commit setup

---
+++ core/src/main/java/com/mygdx/slashtrainer/SlashTrainerGame.java
@@ -0,0 +1,7 @@
+package com.mygdx.slashtrainer;
+
+import com.badlogic.gdx.ApplicationAdapter;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class SlashTrainerGame extends ApplicationAdapter {
+}
